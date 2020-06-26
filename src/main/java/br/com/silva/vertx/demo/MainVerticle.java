9b1223cc684688cd676fb92693ce76e9434b0cac
Danilo da Silva Pereira <dansilva41@hotmail.com>
2020-06-26 11:46:02 -0300

    Handler router by method reference

--- src/main/java/br/com/silva/vertx/demo/MainVerticle.java
+++ src/main/java/br/com/silva/vertx/demo/MainVerticle.java
@@ -2,21 +2,25 @@ package br.com.silva.vertx.demo;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start() {
     Router router = Router.router(vertx);
-    router.get("/api/v1/hello").handler(ctx ->
-      ctx.request().response().end("Hello, I'm trying Vert.x with routers")
-    );
-    router.get("/api/v1/hello/:name").handler(ctx -> {
-      String name = ctx.pathParam("name");
-      ctx.request().response().end(String.format("Hello %s, you're trying Vert.x with routers", name));
-    });
+    router.get("/api/v1/hello").handler(this::helloVertx);
+    router.get("/api/v1/hello/:name").handler(this::helloName);
 
     vertx.createHttpServer().requestHandler(router).listen(8280);
   }
 
+  private void helloVertx(RoutingContext routingContext) {
+    routingContext.request().response().end("Hello, I'm trying Vert.x with routers");
+  }
+
+  private void helloName(RoutingContext routingContext) {
+    String name = routingContext.pathParam("name");
+    routingContext.request().response().end(String.format("Hello %s, you're trying Vert.x with routers", name));
+  }
 }
