4f4697c910f0cd86369bf4fe705f0c991e7b977a
Danilo da Silva Pereira <dansilva41@hotmail.com>
2020-06-26 18:24:12 -0300

    Static Content and auth

--- src/main/java/br/com/silva/vertx/demo/MainVerticle.java
+++ src/main/java/br/com/silva/vertx/demo/MainVerticle.java
@@ -2,39 +2,76 @@ package br.com.silva.vertx.demo;
 
 import br.com.silva.vertx.demo.handler.HelloHandler;
 import br.com.silva.vertx.demo.verticle.HelloVerticle;
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.StaticHandler;
 
 import java.util.UUID;
 
 public class MainVerticle extends AbstractVerticle {
 
-  private static final int DEFAULT_HTTP_PORT = 8290;
   private final String verticleId = UUID.randomUUID().toString();
 
   @Override
-  public void start() {
+  public void start(Promise<Void> start) {
     Router router = Router.router(vertx);
+
+    router.route().handler(StaticHandler.create("web").setIndexPage("index.html"));
+
+    router.route().handler(this::createIndex);
     router.get("/api/v1/hello").handler(HelloHandler::helloVertx);
     router.get("/api/v1/hello/:name").handler(HelloHandler::helloName);
 
     this.deployVerticles();
+    this.configureServer(router, start);
+  }
 
-    this.configureServer(router);
+  private void createIndex(RoutingContext routingContext) {
+    String authToken = routingContext.request().getHeader("AUTH_TOKEN");
+    if ("mysecretAuthToken".contentEquals(authToken)) {
+      routingContext.next();
+    } else {
+      routingContext.response().setStatusCode(401).setStatusMessage("UNAUTHORIZED").end();
+    }
   }
 
   private void deployVerticles() {
     vertx.deployVerticle(new HelloVerticle(verticleId));
   }
 
-  private void configureServer(Router router) {
-    int httpPort;
-    try {
-      httpPort = Integer.parseInt(System.getProperty("http.port", String.valueOf(DEFAULT_HTTP_PORT)));
-    } catch (NumberFormatException npe) {
-      httpPort = DEFAULT_HTTP_PORT;
-    }
+  private void configureServer(Router router, Promise<Void> start) {
 
-    vertx.createHttpServer().requestHandler(router).listen(httpPort);
+    ConfigStoreOptions defaultOptions = new ConfigStoreOptions()
+      .setType("file")
+      .setFormat("json")
+      .setConfig(new JsonObject().put("path", "config.json"));
+
+    ConfigRetrieverOptions retrieverOptions = new ConfigRetrieverOptions()
+      .addStore(defaultOptions);
+
+    ConfigRetriever configRetriever = ConfigRetriever.create(vertx, retrieverOptions);
+
+    Handler<AsyncResult<JsonObject>> handler = asyncResult -> this.handleConfigResults(asyncResult, start, router);
+    configRetriever.getConfig(handler);
+  }
+
+  private void handleConfigResults(AsyncResult<JsonObject> asyncResult, Promise<Void> start, Router router) {
+    if (asyncResult.succeeded()) {
+      JsonObject config = asyncResult.result();
+      JsonObject http = config.getJsonObject("http");
+      int port = http.getInteger("port");
+      vertx.createHttpServer().requestHandler(router).listen(port);
+      start.complete();
+    } else {
+      start.fail("Unable to load configuration.");
+    }
   }
 }
